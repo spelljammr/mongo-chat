c3b5758382ed76bdf02ce40d5f1ae8d482b93b31
ahydorn <adamhydorn@gmail.com>
2019-08-01 09:57:47 -0700

    comments

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db) {
 
   console.log('MongoDB connected...');
 
-  // Connect to Socket.io
+  // Connect to 'Socket.io'
   client.on('connection', function(socket) {
     let chat = db.collection('chats');
 
